223f94960e2221e3a2e3d1cf957bfbb164225e49
Fjayala333 <francisco_a333@hotmail.com>
2021-11-30 13:36:15 -0800

    code

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let cards = document.querySelectorAll(".card");
+
+cards.forEach( card => {
+    let moreInfo = card.querySelector(".more-info");
+
+    card.addEventListener('click', e => {
+        if(e.target.classList == "more-info") {
+            card.classList.toggle('is-flipped');
+        }
+    });
+
+    window.addEventListener('click', e => {
+        if (!e.target.classList.contains('more-info')) {
+            card.classList.remove('is-flipped');
+        }
+    });
+});
